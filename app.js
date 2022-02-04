3ae89c609e9ce80967e6795c4290160cc6cc569e
MoySantanaV <92700261+MoySantanaV@users.noreply.github.com>
2022-02-03 20:58:27 -0600

    make it better

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ server.listen(port, () => {
 const express = require('express')
 const app = express()
 
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.set('view engine', 'ejs')
 app.set('views',__dirname + '/views')
