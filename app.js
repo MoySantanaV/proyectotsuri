cd1da7a935467dcea4df5b094bcf06c46df54523
MoySantanaV <92700261+MoySantanaV@users.noreply.github.com>
2022-02-04 21:22:09 -0600

    prueba3

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 
-const port = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
 // conexion a base de datos
 const mongoose = require('mongoose');
@@ -35,6 +35,6 @@ app.use((req, res, next) => {
     res.status(404).render('404')
 })
 
-app.listen(port, () => {
-    console.log('servidor a su servicio en el puerto', port);
+app.listen(PORT, () => {
+    console.log('servidor a su servicio en el puerto', PORT);
 }) 
