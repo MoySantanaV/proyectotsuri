f74117f9126d66e542196c5625c189df2c737b4b
MoySantanaV <92700261+MoySantanaV@users.noreply.github.com>
2022-02-05 23:37:33 -0600

    botones

--- router/proyeccionesRadiologicas.js
+++ router/proyeccionesRadiologicas.js
@@ -22,6 +22,43 @@ router.get('/', async (req, res) => {
 
 })
 
+router.get('/crear', (req, res) => {
+    res.render('crear')
+})
+
+router.post('/', async (req, res) => {
+    const body = req.body
+    try {
+
+        await proyeccionRadiologica.create(body)
+
+        res.redirect('/proyeccionesRadiologicas')
+        
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+router.get('/:id', async (req, res) => {
+    
+    const id = req.params.id
+
+    try {
+        
+        const proyeccionRadiologicadb = await proyeccionRadiologica.findOne({_id: id})
+        res.render('detalle', {
+            proyeccionRadiologica: proyeccionRadiologicadb,
+            error: false
+        })
+    } catch (error) {
+        console.log(error)
+        res.render('detalle', {
+        error: true,
+        mensaje: 'No se encuentra el id'           
+        })
+
+    }
+} )
 
 
 module.exports = router
